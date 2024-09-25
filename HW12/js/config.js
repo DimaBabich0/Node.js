52dbc7c4e4ad0cb32d52a5df106fb41199065a5f
Dima Babich <dimavernigor7@gmail.com>
2024-09-25 21:03:25 +0300

    Uploaded HW12

---
+++ HW12/js/config.js
@@ -0,0 +1,20 @@
+﻿var mssql = require('mssql');
+
+var config = {
+	user: 'admin',
+	password: 'admin',
+	server: 'DESKTOP-REVRHUK',
+	database: 'testdb',
+	port: 1433,
+	options: {
+		encrypt: true,
+		trustServerCertificate: true
+	}
+}
+
+var connection = new mssql.ConnectionPool(config);
+var pool = connection.connect(function(err) {
+	if (err) console.log(err)
+}); 
+
+module.exports = pool; 
---
+++ HW12/js/displayhandler.js
@@ -0,0 +1,8 @@
+﻿var mssql = require('mssql');
+var queries = require('./queries');
+
+module.exports = {
+    displayItems: function (req, res) {
+        var query = queries.getAllItems(req, res)
+    }
+};
